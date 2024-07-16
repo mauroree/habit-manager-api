268bd9778986a045d12e23eebde052140d0d17d2
Mauro Ré <82146437+mauroree@users.noreply.github.com>
2024-07-16 14:36:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-app.use(cors());
+// Configuração do CORS
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://habit-manager-app.vercel.app'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true 
+}));
+
 app.use(express.json());
 
 const authRoutes = require('./routes/authRoutes');
@@ -27,15 +33,13 @@ mongoose.connect(MONGODB_URI)
   })
   .catch(err => {
     console.error('Erro ao conectar ao MongoDB:', err);
-    process.exit(1); 
+    process.exit(1);
   });
-
 
 app.use((err, req, res, next) => {
   console.error('Erro no servidor:', err);
   res.status(500).json({ error: 'Erro interno no servidor' });
 });
-
 
 app.use((req, res) => {
   res.status(404).json({ error: 'Rota não encontrada' });
